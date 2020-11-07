464bfd3406cf7706db51b77d01f64022356f57a7
Kendrick Tan <kendricktan0814@gmail.com>
2020-11-07 20:14:24 +1000

    compiles

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,108 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import           EVM.Dapp     (DappInfo, dappInfo)
-import           EVM.Solidity (SourceCache (..))
+import           EVM.Dapp                         (DappInfo, dappInfo)
+import           EVM.Solidity                     (SourceCache (..))
+import           EVM.Symbolic                     (len, litAddr, w256lit)
+import           EVM.Types
 
-import           Data.Monoid  (mempty)
+import           Control.Lens                     (set, (&))
+import           Control.Monad                    (void)
+import           Control.Monad.Trans.State.Strict
+import           Data.ByteString                  (ByteString)
+import           Data.Maybe
+import           Data.Monoid                      (mempty)
+import           Data.SBV                         (literal)
 
 import qualified EVM
+import qualified EVM.FeeSchedule                  as FeeSchedule
+import qualified EVM.Fetch
 import qualified EVM.Stepper
+import qualified EVM.VMTest                       as VMTest
 
+-- txhash 0x44c6f3f304a1e566c14063066f440d82eb7adbae57a3b9bcb1aec7c7dab65766
 emptyDapp :: DappInfo
 emptyDapp = dappInfo "" mempty (SourceCache mempty mempty mempty mempty)
+
+caller :: Addr
+caller = read "0x7ee39071c8e207bea9ed01d27d195f3a6cc886ac"
+
+address :: Addr
+address = read "0x7a250d5630b4cf539739df2c5dacb4c659f2488d"
+
+url = "http://localhost:8545"
+
+blockNum = 11209179
+
+block = EVM.Fetch.BlockNumber blockNum
+
+fetcher = EVM.Fetch.http block url
+
+contract = EVM.Fetch.fetchContractFrom block url address
+
+calldata :: ByteString
+calldata =
+  read
+    "0x18cbafe500000000000000000000000000000000000000000000000000000000283828e300000000000000000000000000000000000000000000000014749f446f3e3b6700000000000000000000000000000000000000000000000000000000000000a00000000000000000000000007ee39071c8e207bea9ed01d27d195f3a6cc886ac000000000000000000000000000000000000000000000000000000005fa65c8e0000000000000000000000000000000000000000000000000000000000000002000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+
+codeType = EVM.RuntimeCode
+
+decipher = hexByteString "bytes" . strip0x
+
+block' = block
+
+value' = 0
+
+caller' = caller
+
+origin' = caller -- Maybe change
+
+calldata' = ConcreteBuffer calldata
+
+address' = address
+
+vm0 miner ts blockNum diff c =
+  EVM.makeVm $
+  EVM.VMOpts
+    { EVM.vmoptContract = c
+    , EVM.vmoptCalldata = (calldata', literal . num $ len calldata')
+    , EVM.vmoptValue = w256lit value'
+    , EVM.vmoptAddress = address'
+    , EVM.vmoptCaller = litAddr caller'
+    , EVM.vmoptOrigin = origin'
+    , EVM.vmoptGas = W256 23000000000
+    , EVM.vmoptGaslimit = W256 2000000
+    , EVM.vmoptCoinbase = 0
+    , EVM.vmoptNumber = 11209179
+    , EVM.vmoptTimestamp = w256lit $ W256 1604741
+    , EVM.vmoptBlockGaslimit = 0
+    , EVM.vmoptGasprice = W256 23000000000
+    , EVM.vmoptMaxCodeSize = 0xffffffff
+    , EVM.vmoptDifficulty = diff
+    , EVM.vmoptSchedule = FeeSchedule.istanbul
+    , EVM.vmoptChainId = 1
+    , EVM.vmoptCreate = False
+    , EVM.vmoptStorageModel = EVM.ConcreteS
+    }
+
+miner = 0
+
+timestamp = 1604743
+
+diff = 0
+
+vm = do
+  contract' <-
+    contract >>= \case
+      Nothing -> error $ "No contract found at " <> show address
+      Just c -> return c
+  return $ VMTest.initTx $ vm0 miner timestamp blockNum diff contract'
+
+runVM vm =
+  execStateT (EVM.Stepper.interpret fetcher . void $ EVM.Stepper.execFully) vm
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
