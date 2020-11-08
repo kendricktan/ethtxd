40ba12aef3c8c82a169f5779427a184c7b576c25
Kendrick Tan <kendricktan0814@gmail.com>
2020-11-08 18:03:16 +1000

    mvp?

--- src/Main.hs
+++ src/Main.hs
@@ -7,13 +7,13 @@ module Main where
 import           EVM.Dapp                         (DappInfo, dappInfo)
 import           EVM.Format                       (showTraceTree)
 import           EVM.Solidity                     (SourceCache (..))
-import           EVM.Symbolic                     (len, litAddr, w256lit)
+import           EVM.Symbolic                     (forceLitBytes, len, litAddr,
+                                                   w256lit)
 import           EVM.Types
 
+import           Control.Lens.Combinators         (view)
 import           Control.Monad                    (void)
 import           Control.Monad.Trans.State.Strict
-import           Data.ByteString                  (ByteString)
-
 import           Data.Monoid                      (mempty)
 import           Data.SBV                         (literal)
 import           Data.Text.IO                     (hPutStr)
@@ -30,14 +30,17 @@ emptyDapp :: DappInfo
 emptyDapp = dappInfo "" mempty (SourceCache mempty mempty mempty mempty)
 
 caller :: Addr
-caller = read "0x7ee39071c8e207bea9ed01d27d195f3a6cc886ac"
+caller = Addr 0xaf6ac9e4b68fb3da8fb95fac1e072a2917b4f78f
 
 address :: Addr
-address = read "0x7a250d5630b4cf539739df2c5dacb4c659f2488d"
+address = Addr 0x7a250d5630b4cf539739df2c5dacb4c659f2488d
+
+ethrunAddress :: Addr
+ethrunAddress = Addr 0x00a329c0648769a73afac7f9381e08fb43dbea72
 
 url = "http://localhost:8545"
 
-blockNum = 11209179
+blockNum = 11106093
 
 block = EVM.Fetch.BlockNumber blockNum
 
@@ -45,25 +48,16 @@ fetcher = EVM.Fetch.http block url
 
 contract = EVM.Fetch.fetchContractFrom block url address
 
-calldata :: ByteString
 calldata =
-  "0x18cbafe500000000000000000000000000000000000000000000000000000000283828e300000000000000000000000000000000000000000000000014749f446f3e3b6700000000000000000000000000000000000000000000000000000000000000a00000000000000000000000007ee39071c8e207bea9ed01d27d195f3a6cc886ac000000000000000000000000000000000000000000000000000000005fa65c8e0000000000000000000000000000000000000000000000000000000000000002000000000000000000000000dac17f958d2ee523a2206206994597c13d831ec7000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+  ConcreteBuffer $
+  decipher
+    "0x38ed173900000000000000000000000000000000000000000000006c3f9032c962aeef5900000000000000000000000000000000000000000000002c1f7f98d3e8ba2df200000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000af6ac9e4b68fb3da8fb95fac1e072a2917b4f78f000000000000000000000000000000000000000000000000000000005f9185350000000000000000000000000000000000000000000000000000000000000003000000000000000000000000d533a949740bb3306d119cc777fa900ba034cd52000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc20000000000000000000000006b175474e89094c44da98b954eedeac495271d0f"
 
 codeType = EVM.RuntimeCode
 
 decipher = hexByteString "bytes" . strip0x
 
-block' = block
-
-value' = 0
-
-caller' = caller
-
-origin' = caller -- Maybe change
-
-calldata' = ConcreteBuffer calldata
-
-address' = address
+value = 0
 
 miner = 0
 
@@ -75,18 +69,18 @@ vm0 miner ts blockNum diff c =
   EVM.makeVm $
   EVM.VMOpts
     { EVM.vmoptContract = c
-    , EVM.vmoptCalldata = (calldata', literal . num $ len calldata')
-    , EVM.vmoptValue = w256lit value'
-    , EVM.vmoptAddress = address'
-    , EVM.vmoptCaller = litAddr caller'
-    , EVM.vmoptOrigin = origin'
-    , EVM.vmoptGas = 0xffffffffffffffff
-    , EVM.vmoptGaslimit = 0xffffffffffffffff
+    , EVM.vmoptCalldata = (calldata, literal . num $ len calldata)
+    , EVM.vmoptValue = w256lit value
+    , EVM.vmoptAddress = address
+    , EVM.vmoptCaller = litAddr caller
+    , EVM.vmoptOrigin = 0
+    , EVM.vmoptGas = 0xffffffff
+    , EVM.vmoptGaslimit = 0xffffffff
     , EVM.vmoptCoinbase = miner
     , EVM.vmoptNumber = blockNum
-    , EVM.vmoptTimestamp = ts
-    , EVM.vmoptBlockGaslimit = 1000000000000
-    , EVM.vmoptGasprice = 1
+    , EVM.vmoptTimestamp = w256lit ts
+    , EVM.vmoptBlockGaslimit = 0
+    , EVM.vmoptGasprice = 0
     , EVM.vmoptMaxCodeSize = 0xffffffff
     , EVM.vmoptDifficulty = diff
     , EVM.vmoptSchedule = FeeSchedule.istanbul
@@ -109,4 +103,19 @@ main :: IO ()
 main = do
   vm' <- vm >>= runVM
   hPutStr stderr (showTraceTree emptyDapp vm')
+  case view EVM.result vm' of
+    Nothing -> error "internal error; no EVM result"
+    Just (EVM.VMFailure (EVM.Revert msg)) -> do
+      print $ ByteStringS msg
+      print "no good"
+    Just (EVM.VMFailure err) -> do
+      print err
+      print "no good"
+    Just (EVM.VMSuccess buf) -> do
+      let msg =
+            case buf of
+              SymbolicBuffer msg' -> forceLitBytes msg'
+              ConcreteBuffer msg' -> msg'
+      print $ ByteStringS msg
+      print "we good"
   return ()
